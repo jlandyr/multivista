b00f8521852e7995d7b165ba85791a674b2cc66f
Pedro Jose <macintoshmac@macbook-pro-de-macintosh.local>
2016-03-28 22:54:41 -0500

    Initial Commit

---
+++ pizzaTests/pizzaTests.swift
@@ -0,0 +1,36 @@
+//
+//  pizzaTests.swift
+//  pizzaTests
+//
+//  Created by Pedro Jose on 28/3/16.
+//  Copyright © 2016 eureka. All rights reserved.
+//
+
+import XCTest
+@testable import pizza
+
+class pizzaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
